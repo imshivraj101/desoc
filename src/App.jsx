00ffaf7f7e6fd6e1db14eab10b430e26a11e2658
imshivraj101 <shivrajtalekar101@gmail.com>
2025-07-18 13:05:55 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
 
       <Navbar/>
       <Hero/>
-
+      <Footer/>
 
 
     </div>
