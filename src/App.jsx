b74419e45fbe441d551bc714ae879902f4842c8a
anni02th <aniketmore2000th@gmail.com>
2025-07-18 23:10:50 +0530

    update and resolving conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import { AuthProvider } from "./context/AuthContext"
+import './index.css'
 
-const App = () => {
+function App() {
   return (
-    <div>
+    <Router>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Hero />} />
+            {/* Add more routes as needed */} 
+        </Routes>
 
-
-
-      <Navbar/>
-      <Hero/>
+        {/* </AuthProvider> */}
+        {/* 1-2 csd
+        1st round maths and array
+        2nd round arrays high
+        3rd round arrays and strings
+        5th week dp */}
       <Footer/>
-
-
-    </div>
+    </Router>
   )
 }
 
