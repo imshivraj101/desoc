e94154c0291aff38b3c811a60c0c1e6f62403fce
anni02th <aniketmore2000th@gmail.com>
2025-07-18 23:00:40 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 // import { AuthProvider } from "./context/AuthContext"
-import './index.css' 
+import './index.css'
 
 function App() {
   return (
@@ -23,25 +22,6 @@ function App() {
         5th week dp */}
       <Footer/>
     </Router>
-=======
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
-
-const App = () => {
-  return (
-    <div>
-
-
-
-      <Navbar/>
-      <Hero/>
-      <Footer/>
-
-
-    </div>
->>>>>>> 00ffaf7f7e6fd6e1db14eab10b430e26a11e2658
   )
 }
 
